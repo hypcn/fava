ec46ab9570b110777c92db0a7d154093817f2c5b
Ed Johnson <ed@neon.fish>
2023-05-31 15:19:26 +0100

    Begin adding client tests

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,8 +1,54 @@
+import { SimpleLogger } from "@hypericon/axe";
+import urljoin from "url-join";
+import { Fava } from "../src";
+import { FavaClient } from "../src/client";
 
-describe("Client", () => {
+const testLogger: SimpleLogger = {
+  error: (...mgs: any[]) => { /* noop */ },
+  debug: (...mgs: any[]) => { /* noop */ },
+  log: (...mgs: any[]) => { /* noop */ },
+  warn: (...mgs: any[]) => { /* noop */ },
+  verbose: (...mgs: any[]) => { /* noop */ },
+};
 
-  test("", () => {
-    // 
+describe("Fava client", () => {
+
+  test("can be created", () => {
+    const favaClient = new FavaClient({});
+    expect(favaClient).toBeDefined();
+  });
+
+  test("can be created with options", () => {
+    const origin = "http://example.com";
+    const routePrefix = "a/route/prefix";
+
+    const favaClient = new FavaClient({
+      origin: "http://example.com",
+      routePrefix: "a/route/prefix",
+    });
+
+    expect(favaClient).toBeDefined();
+    expect(favaClient.apiPrefix).toBe(urljoin(origin, routePrefix, "api"));
+  });
+
+  test("calls the Fava HTTP API", async () => {
+
+    const fava = new Fava({
+      getLogger: (_) => testLogger,
+      http: true,
+      locations: [],
+    });
+    await fava.onReady;
+
+    const favaClient = new FavaClient({
+      origin: "http://example.com",
+      routePrefix: "a/route/prefix",
+    });
+
+
+
+
+    await fava.destroy();
   });
 
 });
